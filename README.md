74d18d33008aefaa8c2b7d898f2fa110e7cfded5
Steve Corbett <corbett@internode.on.net>
2020-06-17 11:05:27 +1000

    Working Demonstration

--- README.md
+++ README.md
@@ -2,23 +2,31 @@
 
 ## Project setup
 ```
-yarn install
+npm install
 ```
 
 ### Compiles and hot-reloads for development
 ```
-yarn serve
+npm run serve
 ```
 
 ### Compiles and minifies for production
 ```
-yarn build
+npm run build
 ```
 
 ### Lints and fixes files
 ```
-yarn lint
+npm run lint
 ```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+## References
+1.	https://code.visualstudio.com/
+2.	https://nodejs.org/en/docs/ and https://nodejs.org/en/download/
+3.	https://vuejs.org/v2/guide/installation.html
+4.	https://www.taniarascia.com/getting-started-with-vue/
+5.	https://vuetifyjs.com/en/
+6.	https://www.usgs.gov/natural-hazards/earthquake-hazards/earthquakes and https://earthquake.usgs.gov/earthquakes/feed/ and https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
+7.	https://github.com/babel/babel 
+8.	https://github.com/axios/axios
+
---
+++ src/components/Earthquakes.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-container>
+    <v-layout column align>
+      <v-row xs12>
+        <v-col cols="12">
+          <div class="text-h4">{{title}}</div>
+        </v-col>
+      </v-row>
+      <v-row xs12>
+        <v-flex xs10 sm8 md5 lg3>
+          <v-text-field v-model="filter" label="Filter" @input="filterResults" clearable></v-text-field>
+        </v-flex>
+        <v-list subheader three-line xs9 sm6 md4 lg3>
+          <div v-for="feature in features" :key="feature.id">
+            <v-list-item v-if="feature.visible">
+              <v-list-item-content>
+                <v-list-item-title>
+                  <a :href="feature.url" target="_blank" rel="noopener noreferrer">{{feature.title}}</a>
+                </v-list-item-title>
+                <v-list-item-subtitle>
+                  Magnitude: {{feature.mag}}
+                  <br />
+                  Time: {{feature.time}}
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </div>
+        </v-list>
+      </v-row>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  mounted() {
+    axios
+      .get(
+        "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson"
+      )
+      .then(response => {
+        this.info = response;
+        this.title = response.data.metadata.title;
+        response.data.features.forEach(feature => {
+          this.features.push({
+            id: feature.properties.id,
+            title: feature.properties.title,
+            mag: feature.properties.mag,
+            time: new Date(feature.properties.time).toISOString(),
+            url: feature.properties.url,
+            visible: true
+          });
+        });
+      })
+      .catch(error => console.log(error));
+  },
+  data() {
+    return {
+      title: "Earthquakes!",
+      features: [],
+      filter: null,
+      info: null
+    };
+  },
+  methods: {
+    filterResults() {
+      this.features.forEach(feature => {
+        if (this.filter == null) {
+          feature.visible = true;
+        } else {
+          const title = feature.title;
+          feature.visible = title
+            .toLowerCase()
+            .includes(this.filter.toLowerCase());
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
